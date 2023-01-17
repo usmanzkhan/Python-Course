73b20567531eeefe9a0cdc6b5771fc5deafc9137
usmankhan75 <usman.khan@primehorizongroup.com>
2023-01-17 13:08:10 +0000

    section1completion

---
+++ Section-1/CelsiustoFarenheit.py
@@ -0,0 +1,4 @@
+celcius=int(input("Enter value in Celcius: "))
+farenheit=(celcius * 9/5) +32
+
+print(f"Value in Farenheit is {farenheit}")
---
+++ Section-1/GrossPay.py
@@ -0,0 +1,4 @@
+hours =int(input("Enter number of hours worked: "))
+rateofpay=float(input("Enter Rate of Pay: "))
+
+print(f"Gross pay is {hours * rateofpay}")
